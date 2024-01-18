e3fd662a2d7953d71b49b5e9d49edaf1cd5ce7e1
Luka <lukams03b@gmail.com>
2024-01-18 15:13:44 -0500

    feat: Finished milestone 1

--- mcmerch.py
+++ mcmerch.py
@@ -2,7 +2,7 @@ import sys
 
 def main():
 
-  tax = {"ON": 0.13}
+  tax = {"ON": 0.13, "QC": 0.14975, "MB": 0.05, "NS": 0.15, "BC": 0.05}
 
   #Extract First argument as the price of the item
   price = float(sys.argv[1])
@@ -10,9 +10,11 @@ def main():
   #Extract second arg as the quatitiy
   quantity = int(sys.argv[2])
 
+  prov = sys.argv[3]
+
   pre_tax = price * quantity
 
-  after_tax = pre_tax * (1 + tax["ON"])
+  after_tax = str(pre_tax * (1 + tax[prov]))
 
   print("Price after tax: " + after_tax)
 
