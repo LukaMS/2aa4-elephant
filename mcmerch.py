e3c02b9ca9ed24163de4e1f3700f48b834ef9c66
Luka <lukams03b@gmail.com>
2024-01-18 14:52:46 -0500

    Selling Unitary goods in Ontario (close #1)

---
+++ mcmerch.py
@@ -0,0 +1,20 @@
+import sys
+
+def main():
+
+  tax = {"ON": 0.13}
+
+  #Extract First argument as the price of the item
+  price = float(sys.argv[1])
+
+  #Extract second arg as the quatitiy
+  quantity = int(sys.argv[2])
+
+  pre_tax = price * quantity
+
+  after_tax = pre_tax * (1 + tax["ON"])
+
+  print(after_tax)
+
+if __name__ == '__main__':
+  main()
